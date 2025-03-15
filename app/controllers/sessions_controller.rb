0e9f5f6b8a3db52bd2eb286974c7a3dffc6d66fe
Christopher Chang <christopher.chang1@kellogg.northwestern.edu>
2025-03-15 13:03:18 +0000

    login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,24 @@
 class SessionsController < ApplicationController
+  
   def new
+
   end
 
   def create
+    @user = User.find_by({"email" => params["email"]})
+    if @user != nil
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+        session["user_id"] = @user["id"]
+        flash["notice"] = "hello"
+        redirect_to "/"
+      else
+        flash["notice"] = "Nope"
+        redirect_to "/login"
+      end
+    else
+      flash[""] = "Nope"
+      redirect_to "/login"
+    end
   end
 
   def destroy
