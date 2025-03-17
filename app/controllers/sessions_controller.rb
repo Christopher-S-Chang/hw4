0b159cd36c0bf20871cd77dc24f557c7c66159b5
Christopher Chang <christopher.chang1@kellogg.northwestern.edu>
2025-03-17 01:45:19 +0000

    more bootstrapping

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,7 +16,7 @@ class SessionsController < ApplicationController
         redirect_to "/login"
       end
     else
-      flash[""] = "Nope"
+      flash["notice"] = "Nope"
       redirect_to "/login"
     end
   end
